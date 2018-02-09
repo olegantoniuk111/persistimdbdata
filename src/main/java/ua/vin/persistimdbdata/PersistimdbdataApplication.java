dda7fd077d63eea86841333fd8ea69bbdb44229a
gans <olegantoniuk.oa@gmail.com>
2018-02-09 23:41:24 +0200

    first commit

---
+++ src/main/java/ua/vin/persistimdbdata/PersistimdbdataApplication.java
@@ -0,0 +1,12 @@
+package ua.vin.persistimdbdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistimdbdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistimdbdataApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/vin/persistimdbdata/PersistimdbdataApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.vin.persistimdbdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersistimdbdataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
